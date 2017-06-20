9fa80942051c7366b9a886eb646895ea57611482
JsonTextfield <tehblackboy@gmail.com>
2017-06-20 13:00:17 -0400

    profiles, colours, mixups

--- Ottawa Basketball Courts/ViewController.h
+++ Ottawa Basketball Courts/ViewController.h
@@ -8,9 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
-#import <CoreLocation/CoreLocation.h>
 @interface ViewController : UIViewController
-@property (nonatomic,retain) CLLocationManager *locationManager;
 @property (strong, nonatomic) IBOutlet MKMapView *mapView;
 - (IBAction)selectionChanged:(UISegmentedControl *)sender;
 @property NSArray* courts;
