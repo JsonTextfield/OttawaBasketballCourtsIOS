534fa4be5cf5bf05473edc0cf8964d821203a31b
JsonTextfield <tehblackboy@gmail.com>
2017-06-24 23:00:52 -0400

    took out some repeated code

--- Ottawa Basketball Courts/ViewController.m
+++ Ottawa Basketball Courts/ViewController.m
@@ -18,12 +18,7 @@ NSString* stringFormat = @"%@ (%@)";
     [super viewDidLoad];
     
     _courts = [MyDB database].courts;
-    for (Court* it in _courts){
-        MKPointAnnotation*  marker = [[MKPointAnnotation alloc]init];
-        marker.coordinate = it.location.coordinate;
-        marker.title = [NSString stringWithFormat:stringFormat, it.name, it.courtType];
-        [_mapView addAnnotation: (marker)];
-    }
+    [self loadAll];
     [_mapView showAnnotations:_mapView.annotations animated:false];
     
     
@@ -34,19 +29,21 @@ NSString* stringFormat = @"%@ (%@)";
     // Dispose of any resources that can be recreated.
 }
 
-
+- (void)loadAll{
+    for (Court* it in _courts){
+        MKPointAnnotation*  marker = [[MKPointAnnotation alloc]init];
+        marker.coordinate = it.location.coordinate;
+        marker.title = [NSString stringWithFormat:stringFormat, it.name, it.courtType];
+        [_mapView addAnnotation: (marker)];
+    }
+}
 
 - (IBAction)selectionChanged:(UISegmentedControl *)sender {
     NSArray* sections = [[NSArray alloc] initWithObjects:@"full",@"all",@"half", nil];
     [_mapView removeAnnotations:_mapView.annotations];
     
     if(sender.selectedSegmentIndex == 1){
-        for (Court* it in _courts){
-            MKPointAnnotation*  marker = [[MKPointAnnotation alloc]init];
-            marker.coordinate = it.location.coordinate;
-            marker.title = [NSString stringWithFormat:stringFormat, it.name, it.courtType];
-            [_mapView addAnnotation: (marker)];
-        }
+        [self loadAll];
         
     }
     
@@ -61,7 +58,7 @@ NSString* stringFormat = @"%@ (%@)";
         }
         
     }
-
+    
     [_mapView showAnnotations:_mapView.annotations animated:true];
 }
 @end
